668e13114d059509715959d4d0357455c9fed44f
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-10-07 01:13:52 +0200

    Add project files.

---
+++ ConstuctorsAndDestructors/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClassAndMethodToString
+{
+    internal class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+        private string Name;
+        public override string ToString()
+        {
+            return Name;
+        }
+        ~Person()
+        {
+            Name = string.Empty;
+        }
+    }
+}
---
+++ FirstClassAndMethodToString/Program.cs
@@ -0,0 +1,24 @@
+﻿using FirstClassAndMethodToString;
+
+Person[] people = new Person[3];
+    Console.WriteLine($"Enter three different names and press enter after each of them!");
+for (int i = 0; i < 3 ; i++)
+{
+
+    Person person = new Person();
+    person.Name = Console.ReadLine() ?? "";
+
+
+
+    people[i] = person;
+
+    
+   
+}
+
+
+Console.WriteLine();
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Hello! my name is {people[i].ToString()}");
+}
